d18b5a2ced0cb5d83773675757b33e4e1ea3af04
Ezer Silva <ezersilva@gmail.com>
2025-08-04 08:30:00 -0300

    update settings

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -1,16 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.routers import main
+from app.routers import deals
 from app.settings import PORT
 
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+app.include_router(main.router)
+app.include_router(deals.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=PORT)
--- backend/app/settings/__init__.py
+++ backend/app/settings/__init__.py
@@ -10,3 +10,8 @@ __all__ = [
 
 
 PORT = env.int("PORT", 8000)
+DB_NAME = env("DB_NAME", "viagemeconomica")
+DB_USER = env("DB_USER", "postgres")
+DB_PASSWORD = env("DB_PASSWORD", "postgres")
+DB_HOST = env("DB_HOST", "localhost")
+DB_PORT = env.int("DB_PORT", 5432)
